07806613c61a0b91a378f1269ed2fbbe14eeba45
PlanetEfficacy <jspevack@gmail.com>
2017-01-05 15:49:56 -0700

    Finished tutorial

---
+++ src/About.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class About extends React.Component {
+
+  goToDestination(e) {
+    e.preventDefault()
+    const destination = this.formInput.value.toLowerCase()
+    console.log("stored input value: " + destination)
+    this.context.router.transitionTo(`/about/${destination}`)
+  }
+
+  render () {
+    return (
+      <div className="About">
+        <h2>About Page</h2>
+
+        <form>
+          <input  type="text"
+                  placeholder="Destination"
+                  ref={(input) => { this.formInput = input }}/>
+          <button onClick={(e) => this.goToDestination(e)}>
+            Transition To Destination
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+About.contextType = {
+  router: React.PropTypes.object
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+
 import App from './App';
 import './index.css';
+import About from './About'
+import Contact from './Contact'
+import NotFound from './NotFound'
+import Navigation from './Navigation'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { BrowserRouter, Match, Miss } from 'react-router'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Match exactly pattern="/" component={App} />
+        <Match pattern="/about" component={About} />
+        <Match pattern="/contact" component={Contact} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector("#root"))
